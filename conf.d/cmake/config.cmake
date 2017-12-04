683e2cab5aeabb99d9b3c428073819366f3db709
Romain Forlot <romain.forlot@iot.bzh>
2017-12-04 23:30:23 +0100

    Fix LUA path not set
    Change-Id: I85f0299c3612fd8cfa39241db05d76c7adbbc86d
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -131,6 +131,7 @@ list(APPEND link_libraries afb-utilities lua-lib)
 set(CONTROL_SUPPORT_LUA 1)
 add_definitions(-DCONTROL_PLUGIN_PATH="${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/lib/plugins:${CMAKE_BINARY_DIR}/package/lib/plugins")
 add_definitions(-DCONTROL_CONFIG_PATH="${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/etc:${CMAKE_BINARY_DIR}/package/etc")
+add_definitions(-DCONTROL_LUA_PATH="${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/data:${CMAKE_BINARY_DIR}/package/data")
 add_definitions(-DCTL_PLUGIN_MAGIC=3286576532)
 
 # (BUG!!!) as PKG_CONFIG_PATH does not work [should be an env variable]

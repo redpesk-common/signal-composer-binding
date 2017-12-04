1cf6b5e10983716b4fd222db559fe7161397e285
Romain Forlot <romain.forlot@iot.bzh>
2017-12-04 17:23:07 +0100

    Disable Efence tool use at build.
    Change-Id: I5d7b3b8b9ac17c15143a20111dd2bcb3c44ab3bf
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -46,7 +46,7 @@ set(PROJECT_APP_TEMPLATES_DIR "conf.d/app-templates")
 # Compilation Mode (DEBUG, RELEASE)
 # ----------------------------------
 set(CMAKE_BUILD_TYPE "DEBUG")
-set(USE_EFENCE 1)
+set(USE_EFENCE 0)
 
 # Kernel selection if needed. You can choose between a
 # mandatory version to impose a minimal version.

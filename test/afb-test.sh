77973a72cb095f23a1e9bc776582781e8316e265
Clément Bénier <clement.benier@iot.bzh>
2018-09-12 14:53:40 +0200

    txc-binding: in conf files, rename api to txc
    fix multiples errors, rename txc into txc-binding
    
    Change-Id: I9cd32cf157e407d98293bc869ed08cc93518a555
    Signed-off-by: Clément Bénier <clement.benier@iot.bzh>

--- test/afb-test.sh
+++ test/afb-test.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/sh -x
 trap "cleanup 0" EXIT
 trap "cleanup 1" SIGINT SIGTERM SIGABRT SIGHUP
 cleanup() {
   trap '' SIGINT SIGTERM SIGABRT SIGHUP EXIT
   kill $AFTESTPID > /dev/null 2>&1
-  rm -f $AFTESTSOCKET
   pkill $PROCNAME
   exit $1
 }
@@ -13,8 +12,7 @@ BINDER=$(command -v afb-daemon)
 AFBTEST="$(pkg-config --variable libdir afb-test)/aft.so"
 if [ ! $? -eq 0 ]; then echo "Set PKG_CONFIG_PATH for afb-test"; exit -1; fi
 PROCNAME="afbd-signal-composer"
-PORT1=1234
-PORT2=1235
+PORT=1234
 TOKEN=
 [ "$1" ] && BUILDDIR="$1" || exit 1
 AFTESTSOCKET=/tmp/signal-composer
@@ -26,24 +24,16 @@ export AFT_PLUGIN_PATH="${TESTPACKAGEDIR}/var:${TESTPACKAGEDIR}/lib/plugins"
 pkill $PROCNAME
 
 ${BINDER} 	--name="${PROCNAME}" \
-            --port="${PORT1}" \
+            --port="${PORT}" \
 			--tracereq=common \
 			--token=${TOKEN} \
-            --rootdir="${TESTPACKAGEDIR}/../package" \
-			--workdir="${TESTPACKAGEDIR}" \
-			--binding="../package/lib/afb-signal-composer.so" \
-            --ws-server=unix:${AFTESTSOCKET} > /dev/null 2>& 1 &
+			--workdir="${BUILDDIR}" \
+            --binding=${AFBTEST} \
+			--binding="package/lib/afb-signal-composer.so" \
+            -vvv \
+            --call="aft-signal-composer/launch_all_tests:{}" \
+            --call="aft-signal-composer/exit:{}"
 AFTESTPID=$!
-
-sleep 0.3
-
-${BINDER} --name=aft-signal-composer \
---workdir="package-test/" \
---port="${PORT2}" \
---binding=${AFBTEST} \
---ws-client=unix:${AFTESTSOCKET}  \
---call="aft-signal-composer/launch_all_tests:{}" \
---call="aft-signal-composer/exit:{}"
 
 find "${BUILDDIR}" -name test_results.log -exec cat {} \;
 
